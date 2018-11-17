528a3cad0df61282f2a6f26d961a97703eb3e0a3
zysdyb <764223477@qq.com>
2018-11-17 17:27:05 +0800

    一级分类开发完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -45,4 +45,62 @@ $(function () {
   }
 
 
+  $('#addBtn').click(function(){
+
+     $('#addModal').modal('show');
+
+  });
+
+
+  $('#form').bootstrapValidator({
+
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    fields:{
+      categoryName:{
+        validators:{
+  
+          notEmpty:{
+            message:"请输入一级分类"
+          }
+          
+        }
+
+      }
+    }
+
+  });
+
+  
+  $('#form').on("success.form.bv",function(e){
+    e.preventDefault();
+
+    $.ajax({
+      type:"post",
+      url:"/category/addTopCategory",
+      data: $("#form").serialize(),
+      dataType:"json",
+      success:function(info){
+         
+        if(info.success){
+ 
+           $("#addModal").modal("hide");
+
+           currentPage = 1;
+
+           render();
+
+           $("#form").data("bootstrapValidator").resetForm( true );
+
+        }
+          
+      }
+
+    })
+  })
+
 })
